21fc4f409b21e09928b8216ae79b020ce0bb7787
Nicklas Spendler <xdanishgamerz@gmail.com>
2019-05-27 13:22:53 +0200

    lavet bedste side 2

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,18 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home', {
-         "titel": "OmegaNiceForside dude"
+         "title": "Home page"
       });
    });
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', {
+         "title": "Contact page"
+      });
    });
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {
+         "title": "Product page",
+         "products": [{"name": "Banan"}, {"name": "Æble"}, {"name": "Appelsin"}]
+      });
    });
 };
